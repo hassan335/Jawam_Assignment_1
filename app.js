891eda9337723434df85d595a2de131d55046532
hassan335 <mohammadhassan3737@gmail.com>
2021-06-26 17:45:07 -0700

    work complete

---
+++ app.js
@@ -0,0 +1,471 @@
+//Assignment 1
+
+// Chapter #1
+
+//Alerts
+
+// 1. Write a script to greet your website visitor using JS alert
+// box
+
+// alert("Hello");
+
+//2. Write a script to display following message on your web
+// page:
+
+// alert("Please Enter a valid password");
+
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+// alert("Welcome to JS Land ..."+"\n"+"Happy Coading!");
+
+// 4. Write a script to display following messages in sequence:
+// alert("Welcome to JS Land...");
+// alert("Happy Coading !");         
+
+// 5. Generate the following message through browser’s
+// developer console:
+// alert("Hello Ican run JS through my web browser's console");
+
+// Chapter #2
+
+// 1. Declare a variable called username.
+//   var username;
+
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+ // var myName = "Mohammad Hassan Siddiqui"
+
+ // 3. Write script to
+ //a) Declare a JS variable, titled message.
+//  b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// var message = "Hello world";
+// alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+// var students_name = "Mohammad Hassan Siddiqui";
+// var age = 23;
+// var course = "Certified Mobile and web application developer";
+// alert(students_name);
+// alert(age);
+// alert(course);
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+// var dish = ['P','I','Z','Z','A'];
+
+// for(var i=dish.length; i > 0; i--)
+// {
+//     for(var j=0; j < i  ; j++ )
+//   {
+//     alert(dish[j])  
+// document.write(dish[j])
+//   }
+//   document.write('<br/>');
+  
+
+// }
+
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+// var email = "abc@gmail.com";
+// alert('My email address is \t'+email);
+
+
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+// var book = 'A smarter way to learn JavaScript' ;
+// alert(book);
+
+
+// 8. Write a script to display this in browser through JS
+
+// document.write('Yah! I can write HTML content in browser through Javascript');
+
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+
+// var end = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬' ;
+
+
+// document.write(end)
+// alert(end);
+
+// Chapter 3
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+// var age = 23;
+// alert('Iam'+age+'Years old');
+
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+// var visitorcount=0;
+// ++visitorcount;
+// alert("You have visited this site  "+visitorcount+"\t no of times")
+
+
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+
+// var birthyear = 1997;
+// var datatypevar = typeof(birthyear);
+// document.write('my birth year is '+birthyear+"<br/>"+"datatype of my declared varibale is"+datatypevar)
+
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// V
+
+// var visitorName = 'Mohammad Hassan'
+// var productTitle ='Shirt (s)';
+// var order =6;
+
+// document.write(visitorName+'\t ordered \t'+order+"\t"+productTitle+'\t On our ABC clothing store');
+
+
+// Chapter 4
+
+// 1. Declare 3 variables in one statement.
+// var name,age,gender;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+// var _name,a_ge,gender1,education$,$money;
+// var 6name,a-ge,gender%,education^,money+;
+
+
+    // 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+// document.write('A heading stating “Rules for naming JS variables” <br/>')
+// document.write('Variable names can only contain <u> numbers </u> , <u> $ </u> and <u> _ </u> <br/>');
+//  document.write('Variables must begin with a <u> letter </u>   or <u> $ </u> For example $name, _name or name <br/>')
+//  document.write('Variable names are case <u> sensitive </u> <br/>')
+//  document.write('Variable names should not be JS <u> Keyword </u>');
+
+// Chapter 5
+
+// 1. Write a program that take two numbers & add them in a 
+// new variable. Show the result in your browser.
+
+// var a =5
+// var b =10
+// var sum =a+b
+// document.write(sum);
+
+// 2. Repeat task1 for subtraction, multiplication, division &
+// modulus.
+// var a =5
+// var b =10
+// var sum =a-b
+// document.write(sum);
+
+// var a =5
+// var b =10
+// var sum =a/b
+// document.write(sum);
+
+// var a =5
+// var b =10
+// var sum =a*b
+// document.write(sum);
+
+// var a =5
+// var b =10
+// var sum =a%b
+// document.write(sum);
+
+
+// 3. Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial
+// value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// g. Add 7 to the variable
+// h. Show the value of variable in your browser like “Value
+// after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value
+// by 3.
+// l. Output : “The remainder is : 0”.
+
+// var a;
+// document.write("value of a after declaration is \t "+a+'<br/>');
+// a =5;
+// document.write("Initialized it with value \t"+a+'<br/>');
+// a++;
+// document.write('Value after increment is \t '+a+'<br/>');
+// a +=7;
+// document.write(a);
+// a--;
+// document.write('value after decrement is \t'+a+"<br/>");
+// var output = a % 3 ;
+// document.write("the remainder is \t"+output);
+
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+// to a movie. Example output:
+
+// var per_movie_ticket =600;
+// var buying = per_movie_ticket * 5;
+// document.write(buying);
+
+// 5. Write a script to display multiplication table of any
+// number in your browser. E.g
+
+
+// var number = prompt("Enter the numbet");
+// for(var i =1; i <= 10; i++ )
+// {
+    
+//     document.write(number+"x"+i+"\t ="+number*i+"<br/>");
+
+
+// }
+
+
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a
+// converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”
+
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a
+// converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”
+
+
+// var Celsius_temp = 45;
+
+// var farenhite_temp = 70;
+
+// var c = (farenhite_temp - 32 ) * 5 / 9;
+
+// var f = (Celsius_temp * 5 / 9 ) +32 ;
+
+// document.write(Celsius_temp+" C is \t"+f+"F <br/>");
+// document.write(farenhite_temp+" F is \t"+c+"C");
+
+
+
+
+
+
+
+
+
+
+
+
+// chapter 14 
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+// 3. Declare and initialize a strings array.
+// 4. Declare and initialize a numbers array.
+// 5. Declare and initialize a boolean array.
+// 6. Declare and initialize a mixed array.
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+// var aray = [];
+
+// var cars = ['Mira','Corolla','Cultus','Alto'];
+// var nums = [1,2,3,4,5];
+// var flag = [true,false,true,true];
+// var mixed = ['Mira',true,33,false,'Cultus']
+
+// var education = ['SSC','HSC','BCS','BS','BCOM','MS','M.Phil,PhD']
+
+// document.write(education)
+
+
+// 8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:
+
+// var stu_names = ['Hassan','Kamran','Ahsan'];
+// var score = [480,450,420]
+
+// var percentage_stu1 =(score[0] / 500) * 100;
+// var percentage_stu2 =(score[1] / 500) * 100;
+// var percentage_stu3 =(score[2] / 500) * 100;
+// document.write("Score of"+stu_names[0]+'is'+score[0]+"and percentage is"+percentage_stu1+'<br/>');
+// document.write("Score of"+stu_names[1]+'is'+score[1]+"and percentage is"+percentage_stu2+'<br/>');
+// document.write("Score of"+stu_names[2]+'is'+score[2]+"and percentage is"+percentage_stu3);
+
+
+// 9. Initialize an array with color names. Display the array
+// elements in your browser.
+// a. Ask the user what color he/she wants to add to the
+// beginning & add that color to the beginning of the array.
+// Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end
+// & add that color to the end of the array. Display the
+// updated array in your browser.
+// c. Add two more color to the beginning of the array.
+// Display the updated array in your browser.
+// d. Delete the first color in the array. Display the updated
+// array in your browser.
+// e. Delete the last color in the array. Display the updated
+// array in your browser.
+// f. Ask the user at which index he/she wants to add a color
+// & color name. Then add the color to desired
+// position/index. . Display the updated array in your
+// browser.
+// g. Ask the user at which index he/she wants to delete
+// color(s) & how many colors he/she wants to delete. Then
+// remove the same number of color(s) from user-defined
+// position/index. . Display the updated array in your
+// browser.
+
+
+
+//  var colornames = ['Red','Yellow','Orange','Pink'];
+//  document.write(colornames);
+//  var add =prompt("What color you want to add in the array begging of the array");
+//  colornames.unshift(add);
+//  document.write(colornames);
+//  var add =prompt("What color you want to add in the array in last position of the array");
+//  colornames.push(add);
+//  document.write(colornames);
+//  var add =prompt("What color you want to add in the array begging of the array");
+//  colornames.unshift(add);
+//  var add =prompt("What color you want to add in the array begging of the array");
+//  colornames.unshift(add);
+//  document.write(colornames);
+//  colornames.shift();
+//  document.write(colornames);
+//  colornames.pop();
+//  document.write(colornames);
+//   var index_no = +prompt('At which index you want to add a color')
+//   var add =prompt("What color you want to add ");
+//   colornames.splice(index_no,1,add);
+//   document.write(colornames)
+// document.write(colornames)
+// var index_no = +prompt('At which index you want to delete a color')
+// var count =+prompt("How many colors you wants to");
+// colornames.splice(index_no,count);
+// document.write(colornames)
+
+// 10. Write a program to store student scores in an array &
+// sort the array in ascending order using Array’s sort
+// method
+
+
+// var score = [200,150,170,180,250]
+// document.write(score);
+// document.write(score.sort())
+
+
+// 11. Write a program to initialize an array with city names.
+// Copy 3 array elements from cities array to selectedCities
+// array.
+
+// var cities = ['karachi','lahore','Newyork','Multan']
+// var selectedCities = [];
+//  selectet_cities = cities.slice(0,3);
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
